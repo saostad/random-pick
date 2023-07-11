690409c44a0e0005b6a4aa7a3bd9fdf85a868f94
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2023-07-11 14:53:01 -0400

    enhance: header elements

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -29,7 +29,7 @@
 					></label
 				></span
 			>
-			<div class="flex items-center gap-2 lg:hidden">
+			<div class="flex items-center gap-2">
 				<a
 					href="/"
 					aria-current="page"
@@ -60,85 +60,10 @@
 					</div></a
 				>
 			</div>
-			<div class="hidden w-full max-w-sm lg:flex">
-				<label class="searchbox relative mx-3 w-full"
-					><svg
-						class="pointer-events-none absolute z-10 my-3.5 ml-4 stroke-current opacity-60 text-base-content"
-						width="16"
-						height="16"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						/></svg
-					>
-					<div
-						data-svelte-typeahead=""
-						role="combobox"
-						aria-haspopup="listbox"
-						aria-controls="typeahead-0.ooavbslgwzn-listbox"
-						aria-expanded="false"
-						id="typeahead-0.ooavbslgwzn-typeahead"
-						class="svelte-4tg1b1"
-					>
-						<form data-svelte-search="">
-							<label
-								id="typeahead-0.ooavbslgwzn-label"
-								for="typeahead-0.ooavbslgwzn"
-								class="svelte-wqugyy">Search</label
-							>
-							<input
-								name="search"
-								type="search"
-								placeholder="Search…"
-								autocomplete="off"
-								spellcheck="false"
-								id="typeahead-0.ooavbslgwzn"
-								aria-autocomplete="list"
-								aria-controls="typeahead-0.ooavbslgwzn-listbox"
-								aria-labelledby="typeahead-0.ooavbslgwzn-label"
-								class="svelte-wqugyy"
-							/>
-						</form>
-						<ul
-							role="listbox"
-							aria-labelledby="typeahead-0.ooavbslgwzn-label"
-							id="typeahead-0.ooavbslgwzn-listbox"
-							class="svelte-4tg1b1 svelte-typeahead-list"
-						/>
-					</div>
-					<div
-						class="pointer-events-none absolute right-10 top-2.5 gap-1 opacity-50 hidden lg:flex"
-					>
-						<kbd class="kbd kbd-sm">⌘</kbd> <kbd class="kbd kbd-sm">K</kbd>
-					</div></label
-				>
-			</div>
 		</div>
 		<div class="flex-0">
-			<div class="hidden flex-none items-center">
-				<a
-					data-sveltekit-preload-data="hover"
-					href="/components/"
-					class="btn btn-ghost drawer-button font-normal normal-case"
-					><svg
-						width="20"
-						height="20"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						class="h-5 w-5 fill-current"
-						><path
-							d="M6.5,22 C4.01471863,22 2,19.9852814 2,17.5 C2,15.0147186 4.01471863,13 6.5,13 C8.98528137,13 11,15.0147186 11,17.5 C11,19.9852814 8.98528137,22 6.5,22 Z M17.5,22 C15.0147186,22 13,19.9852814 13,17.5 C13,15.0147186 15.0147186,13 17.5,13 C19.9852814,13 22,15.0147186 22,17.5 C22,19.9852814 19.9852814,22 17.5,22 Z M6.5,11 C4.01471863,11 2,8.98528137 2,6.5 C2,4.01471863 4.01471863,2 6.5,2 C8.98528137,2 11,4.01471863 11,6.5 C11,8.98528137 8.98528137,11 6.5,11 Z M17.5,11 C15.0147186,11 13,8.98528137 13,6.5 C13,4.01471863 15.0147186,2 17.5,2 C19.9852814,2 22,4.01471863 22,6.5 C22,8.98528137 19.9852814,11 17.5,11 Z M17.5,9 C18.8807119,9 20,7.88071187 20,6.5 C20,5.11928813 18.8807119,4 17.5,4 C16.1192881,4 15,5.11928813 15,6.5 C15,7.88071187 16.1192881,9 17.5,9 Z M6.5,9 C7.88071187,9 9,7.88071187 9,6.5 C9,5.11928813 7.88071187,4 6.5,4 C5.11928813,4 4,5.11928813 4,6.5 C4,7.88071187 5.11928813,9 6.5,9 Z M17.5,20 C18.8807119,20 20,18.8807119 20,17.5 C20,16.1192881 18.8807119,15 17.5,15 C16.1192881,15 15,16.1192881 15,17.5 C15,18.8807119 16.1192881,20 17.5,20 Z M6.5,20 C7.88071187,20 9,18.8807119 9,17.5 C9,16.1192881 7.88071187,15 6.5,15 C5.11928813,15 4,16.1192881 4,17.5 C4,18.8807119 5.11928813,20 6.5,20 Z"
-						/></svg
-					> Components</a
-				>
-			</div>
 			<div title="Change Theme" class="dropdown dropdown-end">
-				<div tabindex="0" class="btn normal-case btn-ghost">
+				<div tabindex="-1" class="btn normal-case btn-ghost">
 					<svg
 						width="20"
 						height="20"
@@ -239,7 +164,7 @@
 				</div>
 			</div>
 			<div title="Change Language" class="dropdown dropdown-end">
-				<div tabindex="0" class="btn btn-ghost normal-case" data-svelte-h="svelte-16sc62l">
+				<div tabindex="-1" class="btn btn-ghost normal-case" data-svelte-h="svelte-16sc62l">
 					<svg
 						class="h-5 w-5 fill-current"
 						xmlns="http://www.w3.org/2000/svg"
@@ -264,7 +189,7 @@
 				<div
 					class="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-16 w-56 overflow-y-auto shadow"
 				>
-					<ul class="menu menu-sm gap-1" tabindex="0">
+					<ul class="menu menu-sm gap-1" tabindex="-1">
 						<li>
 							<button class="active"
 								><img
