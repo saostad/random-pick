dc869c99a319d8e32b0547741bc55c783bf10b59
Sepehr Saljooghi <ssaljooghi5@gmail.com>
2023-07-12 17:06:21 -0400

    changes are in +page.svelte
    - Made random number function
    - Added button to generate this number

--- src/routes/[lang=lang]/products/random-number/+page.svelte
+++ src/routes/[lang=lang]/products/random-number/+page.svelte
@@ -1,2 +1,26 @@
 <h2>Generate a Random Number</h2>
-<p>hi</p>
+
+<script>
+    let randomNumber = 0;      // starting value
+
+    function randoNum() {
+        randomNumber = Math.floor(Math.random() * 100) + 1       
+    }
+
+</script>
+
+
+
+    <div>
+        <button class="btn" on:click={randoNum}>Generate A Random Number</button>
+        <p class="countdown font-mono text-6xl mt-4">{randomNumber}</p>
+    </div>
+
+
+
+
+
+<!-- 
+<span class="countdown font-mono text-6xl">
+    <span style="--value:0;"></span>
+</span> -->
