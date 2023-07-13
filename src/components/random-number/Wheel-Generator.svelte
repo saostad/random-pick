ad049c6517039b900283b76424f77813c7d0d952
Sepehr Saljooghi <ssaljooghi5@gmail.com>
2023-07-13 16:36:36 -0400

    Wheel-Generator.svelte
    - Tried creating an empty array where user inputs different numbers they would like, from there it would generate random pick from the list they inputted.
    
    - Couldn't get it to work for now.

--- src/components/random-number/Wheel-Generator.svelte
+++ src/components/random-number/Wheel-Generator.svelte
@@ -1,1 +1,34 @@
-<h1>Wheel Generator</h1>
+<script>
+    let numbList = [];
+  
+    function addNumberToList() {
+      const input = document.getElementById("input");
+      const number = parseInt(input.value);
+  
+      if (!isNaN(number)) {
+        numbList.push(number);
+        input.value = "";
+      }
+    }
+  
+    function generateNumber() {
+      if (numbList.length > 0) {
+        const randomIndex = Math.floor(Math.random() * numbList.length);
+        const randomNum = numbList[randomIndex];
+        alert("Random Number: " + randomNum);
+      }
+    }
+  </script>
+  
+  <style>
+    button {
+      margin-top: 10px;
+    }
+  </style>
+  
+  <div>
+    <input type="numbList" id="input" placeholder="Enter a number" />
+    <button on:click={addNumberToList}>Add number</button>
+    <div><button on:click={generateNumber}>Generate Random Number</button></div>
+  </div>
+  
