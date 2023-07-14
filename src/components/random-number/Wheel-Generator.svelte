8fd63e705f59da4c05e050d6b2721a5a1f04150b
Sepehr Saljooghi <ssaljooghi5@gmail.com>
2023-07-14 14:57:20 -0400

    Wheel-Generator
    - Numbers that you input will now display on the page
    - "Generate Random Number" only displays it through console and not on the page.

--- src/components/random-number/Wheel-Generator.svelte
+++ src/components/random-number/Wheel-Generator.svelte
@@ -1,34 +1,45 @@
 <script>
-    let numbList = [];
-  
-    function addNumberToList() {
-      const input = document.getElementById("input");
-      const number = parseInt(input.value);
-  
-      if (!isNaN(number)) {
-        numbList.push(number);
-        input.value = "";
-      }
+  let numbList = [];
+
+  function addNumberToList() {
+    const input = document.getElementById("input");
+    const number = parseInt(input.value);
+
+    if (!isNaN(number)) {
+      numbList = [...numbList, number];
+      input.value = "";
     }
-  
-    function generateNumber() {
-      if (numbList.length > 0) {
-        const randomIndex = Math.floor(Math.random() * numbList.length);
-        const randomNum = numbList[randomIndex];
-        alert("Random Number: " + randomNum);
-      }
+  }
+
+  function generateNumber() {
+    if (numbList.length > 0) {
+      const randomIndex = Math.floor(Math.random() * numbList.length);
+      const randomNum = numbList[randomIndex];
+      alert("Random Number: " + randomNum);
     }
-  </script>
-  
-  <style>
-    button {
-      margin-top: 10px;
-    }
-  </style>
-  
+  }
+</script>
+
+<style>
+  button {
+    margin-top: 10px;
+  }
+</style>
+
+<div>
+  <input type="number" id="input" placeholder="Enter a number" />
+  <button on:click={addNumberToList}>Add number</button>
   <div>
-    <input type="numbList" id="input" placeholder="Enter a number" />
-    <button on:click={addNumberToList}>Add number</button>
-    <div><button on:click={generateNumber}>Generate Random Number</button></div>
+    <button on:click={generateNumber}>Generate Random Number</button>
   </div>
-  
+  {#if numbList.length > 0}
+    <div>
+      <h3>Numbers:</h3>
+      <ul>
+        {#each numbList as number}
+          <li>{number}</li>
+        {/each}
+      </ul>
+    </div>
+  {/if}
+</div>
