7c84a716718d1d463f7f2b8bf05ac845d1fd2347
Saeid Ostad <SOstad@kajimausa.com>
2024-05-14 13:55:11 -0400

    chore: Update showVoting initial state to false in VotingSession component

--- app/mafia/components/VotingSession.tsx
+++ app/mafia/components/VotingSession.tsx
@@ -5,8 +5,7 @@ import FlexibleModal from "./FlexibleModal";
 const VotingSession: React.FC = () => {
   const { gameState, decreaseVote, increaseVote, resetVotes } =
     useGameContext();
-
-  const [showVoting, setShowVoting] = useState(true);
+  const [showVoting, setShowVoting] = useState(false);
 
   return (
     <div>
@@ -20,30 +19,31 @@ const VotingSession: React.FC = () => {
       </FlexibleModal>
       <details
         open={showVoting}
-        onClick={() => {
+        onToggle={() => {
           setShowVoting((prev) => !prev);
         }}
       >
         <summary className="secondary" role="button">
-          {!showVoting ? "Hide " : "Show "} Voting Session
+          {showVoting ? "Hide " : "Show "} Voting Session
         </summary>
-
-        <div style={{ marginBottom: "1rem" }}>
-          <button onClick={resetVotes}>Reset Votes</button>
+        <div>
+          <div style={{ marginBottom: "1rem" }}>
+            <button onClick={resetVotes}>Reset Votes</button>
+          </div>
+          <ul>
+            {gameState.players
+              .filter((p) => p.isAlive)
+              .sort((a, b) => a.order - b.order)
+              .map((player) => (
+                <li key={player.id}>
+                  <span style={{ marginRight: "1rem" }}>{player.name} </span>
+                  <button onClick={() => decreaseVote(player.id)}>-</button>
+                  {` Votes: ${player.voteCount} `}
+                  <button onClick={() => increaseVote(player.id)}>+</button>
+                </li>
+              ))}
+          </ul>
         </div>
-        <ul>
-          {gameState.players
-            .filter((p) => p.isAlive)
-            .sort((a, b) => a.order - b.order)
-            .map((player) => (
-              <li key={player.id}>
-                <span style={{ marginRight: "1rem" }}>{player.name} </span>
-                <button onClick={() => decreaseVote(player.id)}>-</button>
-                {` Votes: ${player.voteCount} `}
-                <button onClick={() => increaseVote(player.id)}>+</button>
-              </li>
-            ))}
-        </ul>
       </details>
     </div>
   );
