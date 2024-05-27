a5b53bbcea71e5e8f0781b8935344394060e6128
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-05-27 01:00:23 -0400

    refactor: Adjust ModalButton width to 8 rem

--- app/mafia/components/ModalButton.tsx
+++ app/mafia/components/ModalButton.tsx
@@ -22,7 +22,7 @@ const ModalButton: React.FC<ModalButtonProps> = ({
 
   return (
     <button
-      className="btn btn-ghost btn-outline btn-primary min-w-36 mb-2"
+      className="btn btn-ghost btn-outline btn-primary min-w-32 mb-2"
       onClick={onClick}
       {...props}
     >
