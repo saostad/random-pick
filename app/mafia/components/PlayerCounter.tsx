5d4aeedd9eb7f25e4f51ef9ef8927308e2042fa4
Saeid Ostad <you@example.com>
2024-06-10 13:31:32 -0400

    refactor: Update PlayerCounter component font size to match design

--- app/mafia/components/PlayerCounter.tsx
+++ app/mafia/components/PlayerCounter.tsx
@@ -10,7 +10,7 @@ const PlayerCounter: React.FC = () => {
   return (
     <div className="grid grid-flow-col justify-center gap-5 text-center auto-cols-max">
       <div className="flex flex-col p-2 bg-primary rounded-box text-primary-content justify-items-center">
-        <span className="countdown font-mono text-3xl">
+        <span className="countdown font-mono text-2xl">
           <span
             className="m-auto"
             style={
