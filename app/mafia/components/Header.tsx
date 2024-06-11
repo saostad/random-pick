0ad123000405f245be7de3d2dff825f8ad08e97e
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-11 18:18:44 -0400

    refactor: Update Header component layout and styles for better UI consistency

--- app/mafia/components/Header.tsx
+++ app/mafia/components/Header.tsx
@@ -7,9 +7,9 @@ interface HeaderProps extends HTMLAttributes<HTMLElement> {}
 const Header: React.FC<HeaderProps> = (props) => {
   return (
     <div {...props} className="navbar mt-4">
-      <div className="flex mb-4 flex-1">
+      <div className="flex mb-4 flex-1 [&::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&::selection]:bg-blue-700/20 [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
         <h1 className="btn btn-ghost mr-2 text-4xl font-bold px-2 pt-2">
-          Mafia
+          <span className="">Mafia</span>
         </h1>
         <span className="font-mono self-end max-sm:text-xs">
           Manage you Game!
