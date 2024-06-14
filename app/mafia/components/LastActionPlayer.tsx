f78ddfdde44c4aeab31518ee0f16a4459598dbb6
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-14 01:36:40 -0400

    middel: assigning random action card

--- app/mafia/components/LastActionPlayer.tsx
+++ app/mafia/components/LastActionPlayer.tsx
@@ -1,5 +1,42 @@
-const lastActionPlayer: React.FC = () => {
-  return <p>Last Action for Player X</p>;
+import { useState } from "react";
+import { LastActType, useGameContext } from "../contexts/GameContext";
+
+const LastActionPlayer: React.FC = () => {
+  const { gameState } = useGameContext();
+  const { lastActions } = gameState;
+
+  const [randomCard, setRandomCard] = useState<LastActType>();
+  return (
+    <>
+      <p>Last Action for Player X</p>
+      cards in the game:
+      <ul>
+        {lastActions.map((action, index) => (
+          <li key={action.id}>{action.title}</li>
+        ))}
+      </ul>
+      <div className="divider"></div>
+      <button
+        className="btn btn-secondary btn-outline"
+        onClick={() => {
+          // shuffle cards and assign one
+          const [randomCard] = lastActions
+            .map((el) => el)
+            .sort(() => 0.5 - Math.random());
+
+          setRandomCard(randomCard);
+        }}
+      >
+        Shuffle cards and assign one
+      </button>
+      {randomCard && (
+        <div>
+          <p>Random card assigned:</p>
+          <p>{randomCard.title}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default lastActionPlayer;
+export default LastActionPlayer;
