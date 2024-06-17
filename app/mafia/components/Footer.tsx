ce69c22fc92099694edd22d3e01d5b82aa58be1a
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-16 22:20:15 -0400

    refactor: Update Footer component to use CarbonSettingsCheck icon for settings button

--- app/mafia/components/Footer.tsx
+++ app/mafia/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useGameContext } from "../contexts/GameContext";
 
 import CarbonCloudAuditing from "~icons/carbon/cloud-auditing";
+import CarbonSettingsCheck from "~icons/carbon/settings-check";
 
 interface FooterProps {}
 
@@ -59,20 +60,7 @@ const Footer: React.FC<FooterProps> = () => {
           activeTab === "settings" ? "scale-110" : "scale-100"
         }`}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <CarbonSettingsCheck />
         Settings
       </button>
     </div>
