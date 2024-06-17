0883094655e25e1d4212941912663039ca25dc89
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-16 22:16:10 -0400

    refactor: Update Footer component to use accent color for active tab highlighting

--- app/mafia/components/Footer.tsx
+++ app/mafia/components/Footer.tsx
@@ -18,8 +18,8 @@ const Footer: React.FC<FooterProps> = () => {
     <div className="btm-nav container mx-auto max-w-lg">
       <button
         onClick={() => onNavigate("home")}
-        className={`textarea-info ${
-          activeTab === "home" ? "active" : ""
+        className={`textarea-accent ${
+          activeTab === "home" ? "active text-accent" : ""
         } transition duration-500 ease-in-out transform ${
           activeTab === "home" ? "scale-110" : "scale-100"
         }`}
@@ -42,8 +42,8 @@ const Footer: React.FC<FooterProps> = () => {
       </button>
       <button
         onClick={() => onNavigate("timeline")}
-        className={`textarea-info ${
-          activeTab === "timeline" ? "active" : ""
+        className={`textarea-accent ${
+          activeTab === "timeline" ? "active text-accent" : ""
         } transition duration-500 ease-in-out transform ${
           activeTab === "timeline" ? "scale-110" : "scale-100"
         }`}
@@ -53,8 +53,8 @@ const Footer: React.FC<FooterProps> = () => {
       </button>
       <button
         onClick={() => onNavigate("settings")}
-        className={`textarea-info ${
-          activeTab === "settings" ? "active" : ""
+        className={`textarea-accent ${
+          activeTab === "settings" ? "active text-accent" : ""
         } transition duration-500 ease-in-out transform ${
           activeTab === "settings" ? "scale-110" : "scale-100"
         }`}
