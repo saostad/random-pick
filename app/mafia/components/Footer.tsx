c1c94320feadc015ebf77c442a3f53bed230ede8
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-16 22:25:07 -0400

    refactor: Add IconParkOutlineGamePs to Footer component for game button

--- app/mafia/components/Footer.tsx
+++ app/mafia/components/Footer.tsx
@@ -3,6 +3,7 @@ import { useGameContext } from "../contexts/GameContext";
 
 import CarbonCloudAuditing from "~icons/carbon/cloud-auditing";
 import CarbonSettingsCheck from "~icons/carbon/settings-check";
+import IconParkOutlineGamePs from "~icons/icon-park-outline/game-ps";
 
 interface FooterProps {}
 
@@ -25,20 +26,7 @@ const Footer: React.FC<FooterProps> = () => {
           activeTab === "home" ? "scale-110" : "scale-100"
         }`}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
+        <IconParkOutlineGamePs />
         Game
       </button>
       <button
