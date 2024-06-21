ee5ea291272533afac7d12e7a43b635667d7de66
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-21 10:22:54 -0400

    refactor: Update GameMode component to use "Professional Mode" instead of "Professional Level"

--- app/mafia/components/GameMode.tsx
+++ app/mafia/components/GameMode.tsx
@@ -21,7 +21,7 @@ const GameMode: React.FC = () => {
         <div className="flex flex-col">
           <div className="form-control w-52">
             <label className="cursor-pointer label">
-              <span className="label-text">Professional Level</span>
+              <span className="label-text">Professional Mode</span>
               <input
                 type="checkbox"
                 className="toggle toggle-primary"
