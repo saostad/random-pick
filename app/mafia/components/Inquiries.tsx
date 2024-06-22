8b8626c986c9a416fffb5979aace2ba2f8880a8d
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-06-22 00:35:21 -0400

    refactor: Update Inquiries component to improve UI for displaying dead and active players

--- app/mafia/components/Inquiries.tsx
+++ app/mafia/components/Inquiries.tsx
@@ -47,10 +47,10 @@ const Inquiries: React.FC = () => {
           <div className="divider"></div>
           {deadPlayers.length > 0 && (
             <>
-              <div className="text-xl font-bold mb-4">
-                Dead Players ({deadPlayers.length})
+              <div className="font-bold mb-4">
+                Out of game roles: ({deadPlayers.length})
               </div>
-              <>
+              <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                 {deadPlayers.map((player) => (
                   <div
                     key={player.id}
@@ -61,19 +61,68 @@ const Inquiries: React.FC = () => {
                       marginBottom: "1rem",
                     }}
                   >
-                    (
-                    {
-                      getRoleByPlayerId({
-                        player,
-                        gameRoles: gameState.gameRoles,
-                      })?.name
-                    }
-                    )
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                      </svg>
+                      {
+                        getRoleByPlayerId({
+                          player,
+                          gameRoles: gameState.gameRoles,
+                        })?.name
+                      }
+                    </li>
                   </div>
                 ))}
-              </>
+              </ul>
             </>
           )}
+          <div className="collapse bg-base-200">
+            <input type="checkbox" />
+            <div className="collapse-title font-bold">
+              Roles still in game: ({players.length - deadPlayers.length})
+            </div>
+            <div className="collapse-content">
+              {players.map((player) => {
+                if (deadPlayers.some((dead) => dead.id === player.id)) return;
+                return (
+                  <div
+                    key={player.id}
+                    style={{
+                      display: "grid",
+                      gridTemplateColumns: "1fr 1fr 1fr",
+                      alignItems: "center",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                      </svg>
+                      {
+                        getRoleByPlayerId({
+                          player,
+                          gameRoles: gameState.gameRoles,
+                        })?.name
+                      }
+                    </li>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </>
       )}
     </div>
