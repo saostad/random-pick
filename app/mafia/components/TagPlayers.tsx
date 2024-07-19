2e6c55ecd36cfee949ebbe9864fdb93fe9110f76
Saeid Ostad <16025625+saostad@users.noreply.github.com>
2024-07-19 18:14:08 -0400

    fix: reference problem

--- app/mafia/components/TagPlayers.tsx
+++ app/mafia/components/TagPlayers.tsx
@@ -77,7 +77,7 @@ const TagPlayers: React.FC = () => {
             <>
               <label className="form-control w-full max-w-xs">
                 <div className="label">
-                  <span className="label-text">{t("tag")}</span>
+                  <span className="label-text">{t("NightActions.tag")}</span>
                 </div>
                 <select
                   className="select select-bordered"
